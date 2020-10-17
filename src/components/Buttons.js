fd25a1f49e1526fc9e5fe769cbb61b5ce0dc2270
itsdenispavlovic <pavlovicdenis@icloud.com>
2020-10-17 19:20:14 +0200

    Game of life v1

---
+++ src/components/Buttons.js
@@ -0,0 +1,31 @@
+import React from "react";
+import {ButtonToolbar, Dropdown, DropdownButton} from "react-bootstrap";
+
+class Buttons extends React.Component {
+
+    handleSelect = (event) => {
+        this.props.gridSize(event);
+    }
+
+    render() {
+        return (
+            <div className="center">
+                <ButtonToolbar>
+                    <button className="btn btn-secondary mr-2" onClick={this.props.playButton}>Play</button>
+                    <button className="btn btn-secondary mr-2" onClick={this.props.pauseButton}>Pause</button>
+                    <button className="btn btn-secondary mr-2" onClick={this.props.clear}>Clear</button>
+                    <button className="btn btn-secondary mr-2" onClick={this.props.slow}>Slow</button>
+                    <button className="btn btn-secondary mr-2" onClick={this.props.fast}>Fast</button>
+                    <button className="btn btn-secondary mr-2" onClick={this.props.seed}>Seed</button>
+                    <DropdownButton title="Grid Size" id="size-menu" onSelect={this.handleSelect}>
+                        <Dropdown.Item eventKey="1">20x10</Dropdown.Item>
+                        <Dropdown.Item eventKey="2">50x30</Dropdown.Item>
+                        <Dropdown.Item eventKey="3">70x50</Dropdown.Item>
+                    </DropdownButton>
+                </ButtonToolbar>
+            </div>
+        );
+    }
+}
+
+export default Buttons;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,163 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Import the components
+import Grid from './components/Grid';
+import Buttons from './components/Buttons';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Main extends React.Component {
+
+    constructor() {
+        super();
+
+        this.speed = 100;
+        this.rows = 30;
+        this.cols = 40;
+
+        this.state = {
+            generation: 0,
+            gridFull: Array(this.rows).fill().map(() => Array(this.cols).fill(false))
+
+        }
+    }
+
+    selectBox = (row, col) => {
+        let gridCopy = arrayClone(this.state.gridFull);
+        gridCopy[row][col] = !gridCopy[row][col];
+        this.setState({
+            gridFull: gridCopy
+        });
+    }
+
+    seed = () => {
+        let gridCopy = arrayClone(this.state.gridFull);
+        for(let i =0; i < this.rows; i++) {
+            for (let j = 0; j < this.cols; j++) {
+                if(Math.floor(Math.random() * 4) === 1) {
+                    gridCopy[i][j] = true;
+                }
+            }
+        }
+
+        this.setState({
+            gridFull: gridCopy
+        });
+    }
+
+    playButton = () => {
+        clearInterval(this.intervalId);
+        this.intervalId = setInterval(this.play, this.speed);
+    }
+
+    pauseButton = () => {
+        clearInterval(this.intervalId);
+    }
+
+    slow = () => {
+        this.speed = 1000;
+        this.playButton();
+    }
+
+    fast = () => {
+        this.speed = 100;
+        this.playButton();
+    }
+
+    // Create a function (refractor !)
+    clear = () => {
+        var grid = Array(this.rows).fill().map(() => Array(this.cols).fill(false));
+
+        this.setState({
+            gridFull: grid,
+            generation: 0
+        });
+    }
+
+    gridSize = (no_size) => {
+        switch (no_size) {
+            case "1":
+                this.cols = 20;
+                this.rows = 10;
+            break;
+            case "2":
+                this.cols = 50;
+                this.rows = 30;
+            break;
+            default:
+                this.cols = 70;
+                this.rows = 50;
+        }
+        this.clear();
+    }
+
+    play = () => {
+        let g = this.state.gridFull;
+        let g2 = arrayClone(this.state.gridFull);
+
+        // RULES FOR GAME OF LIFE
+        // Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+        // Any live cell with two or three live neighbours lives on to the next generation.
+        // Any live cell with more than three live neighbours dies, as if by overpopulation.
+        // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+        // Count - how many neighbours it has
+        // If there is a neighbor, we will increase our count with 1
+        // Then we will decide, if it will die or live
+
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.cols; j++) {
+                let count = 0;
+                if (i > 0) if (g[i - 1][j]) count++;
+                if (i > 0 && j > 0) if (g[i - 1][j - 1]) count++;
+                if (i > 0 && j < this.cols - 1) if (g[i - 1][j + 1]) count++;
+                if (j < this.cols - 1) if (g[i][j + 1]) count++;
+                if (j > 0) if (g[i][j - 1]) count++;
+                if (i < this.rows - 1) if (g[i + 1][j]) count++;
+                if (i < this.rows - 1 && j > 0) if (g[i + 1][j - 1]) count++;
+                if (i < this.rows - 1 && j < this.cols - 1) if (g[i + 1][j + 1]) count++;
+                if (g[i][j] && (count < 2 || count > 3)) g2[i][j] = false;
+                if (!g[i][j] && count === 3) g2[i][j] = true;
+            }
+        }
+        this.setState({
+            gridFull: g2,
+            generation: this.state.generation + 1
+        });
+    }
+
+    componentDidMount() {
+        // this.seed();
+        // this.playButton();
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>The Game Of Life</h1>
+                <Buttons
+                    playButton={this.playButton}
+                    pauseButton={this.pauseButton}
+                    slow={this.slow}
+                    fast={this.fast}
+                    clear={this.clear}
+                    seed={this.seed}
+                    gridSize={this.gridSize}
+                />
+                <Grid
+                    gridFull={this.state.gridFull}
+                    rows={this.rows}
+                    cols={this.cols}
+                    selectBox={this.selectBox}
+                />
+                <h2>Generation: {this.state.generation}</h2>
+            </div>
+        );
+    }
+}
+
+function arrayClone(arr) {
+    return JSON.parse(JSON.stringify(arr));
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
